4ab91a53a40950284ece458baef79e5ad3b4542b
Paul Maodi <maodip@infoverge.co.za>
2023-10-10 08:06:50 +0200

    Lesson 2 Done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,14 +4,20 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+  ],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent {
 
 }
